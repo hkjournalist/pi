1f73576338a33976693e89674a43463dd8e61e38
pengpeng119 <wangjianpeng0822@163.com>
2016-12-20 00:30:03 +0800

    second time

---
+++ pi/config.py
@@ -0,0 +1,7 @@
+
+sender = 'wangjianpeng0822@163.com'
+receiver = 'wangjianpeng0822@163.com'
+subject = 'python email test'
+smtpserver = 'smtp.163.com'
+username = 'wangjianpeng0822@163.com'
+password = '19900822a'
